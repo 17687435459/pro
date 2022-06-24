6c0adb3823b084c094563c06ce4b92f3fb7b79c0
番茄 <12385774@qq.com>
2022-06-24 23:10:35 +0800

    lcd12864

--- main.c
+++ main.c
@@ -3,16 +3,26 @@
  * @Date: 2022-06-24 16:40:05
  */
 
-#include <REG51.H>
+#include <reg51.H>
 #include "delay.h"
+#include "key.h"
+#include "lcd12864.h"
 
 sbit led = P2 ^ 2;
-int main()
+void main()
 {
+    Lcd_Init();
+    Lcd_Clear();
     while (1)
     {
-        led=!led;
-        delay_ms(10);
-        led=!led;
+        if (key_cont(key1) == 1)
+        {
+            led = 0;
+        }
+        if (key_cont(key2) == 1)
+        {
+            led = 0;
+        }
+        Lcd_Show_String(0, 0, "hello,world!");
     }
 }
